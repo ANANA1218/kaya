2df99b1a3cf19b0a9b2daf45a92e5fcaa6aa1e96
Annabelle <annabelle.bosse.pro@gmail.com>
2023-11-17 12:33:17 +0100

    first commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\VehiculeRepository; // Assurez-vous d'avoir importé le Repository adéquat
+use App\Entity\Vehicule;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\CommandeRepository; // Assurez-vous d'avoir importé le Repository adéquat
+use App\Entity\Commande;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+
+use Doctrine\DBAL\Types\BooleanType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class AdminController extends AbstractController
+{
+
+    private $vehiculeRepository;
+
+    private $membreRepository;
+    private $commandeRepository;
+
+
+
+    public function __construct(VehiculeRepository $vehiculeRepository, CommandeRepository $commandeRepository)
+    {
+        $this->vehiculeRepository = $vehiculeRepository;
+       
+        $this->commandeRepository = $commandeRepository;
+    }
+
+
+    /**
+     * @Route("/admin", name="admin_home")
+     */
+    public function adminHome(): Response
+    {
+        return $this->render('admin/home.html.twig');
+    }
+
+
+    // vehicules 
+ /**
+     * @Route("/admin/vehicules", name="vehicules_list")
+     */
+    public function vehiculesList(): Response
+    {
+        $vehicules = $this->vehiculeRepository->findAll();
+
+        return $this->render('admin/vehicules_list.html.twig', [
+            'vehicules' => $vehicules,
+        ]);
+    }
+
+
+/**
+     * @Route("/vehicule/{id}", name="show_vehicule")
+     */
+    public function showVehiculeByID($id): Response
+    {
+        $vehicule = $this->vehiculeRepository->find($id);
+
+        return $this->render('admin/vehicule_by_id.html.twig', [
+            'vehicule' => $vehicule,
+        ]);
+    }
+
+
+
+ /**
+     * @Route("/admin/vehicule/create", name="create_vehicule")
+     */
+    public function createVehicule(Request $request, EntityManagerInterface $entityManager): Response
+{
+    // Création d'une nouvelle instance de Vehicule
+    $vehicule = new Vehicule();
+
+    // Création du formulaire
+    $form = $this->createFormBuilder($vehicule)
+        ->add('titre', TextType::class)
+        ->add('marque', TextType::class)
+        ->add('modele', TextType::class)
+        ->add('description', TextareaType::class)
+        ->add('photo', TextType::class)
+        ->add('prixJournalier', NumberType::class)
+        ->add('save', SubmitType::class, ['label' => 'Ajouter'])
+        ->getForm();
+
+    // Gérer la soumission du formulaire
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // Récupération des données du formulaire
+        $vehicule = $form->getData();
+
+        // Enregistrement du véhicule dans la base de données
+        $entityManager->persist($vehicule);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Véhicule ajouté avec succès !');
+
+        // Redirection vers une autre page après l'ajout
+        return $this->redirectToRoute('vehicules_list');
+    }
+
+    // Rendre la vue du formulaire
+    return $this->render('admin/Form/vehicule/create.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+
+/**
+ * @Route("/admin/vehicule/{id}/update", name="update_vehicule")
+ * @ParamConverter("vehicule", class="App\Entity\Vehicule")
+ */
+public function updateVehicule(Request $request, EntityManagerInterface $entityManager, Vehicule $vehicule): Response
+{
+    // Création du formulaire pré-rempli avec les données du véhicule à mettre à jour
+    $form = $this->createFormBuilder($vehicule)
+        ->add('titre', TextType::class)
+        ->add('marque', TextType::class)
+        ->add('modele', TextType::class)
+        ->add('description', TextareaType::class)
+        ->add('photo', TextType::class)
+        ->add('prixJournalier', NumberType::class)
+        ->add('save', SubmitType::class, ['label' => 'Modifier'])
+        ->getForm();
+
+    // Gérer la soumission du formulaire
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        // Enregistrement automatique des modifications dans la base de données via Doctrine
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Véhicule mis à jour avec succès !');
+
+        // Redirection vers une autre page après la mise à jour
+        return $this->redirectToRoute('vehicules_list');
+    }
+
+    // Rendre la vue du formulaire
+    return $this->render('admin/Form/vehicule/update.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
+
+
+
+/**
+ * @Route("/admin/vehicule/{id}/delete", name="delete_vehicule")
+ */
+public function deleteVehicule(Request $request, EntityManagerInterface $entityManager, Vehicule $vehicule): Response
+{
+    $entityManager->remove($vehicule);
+    $entityManager->flush();
+
+    $this->addFlash('success', 'Véhicule supprimé avec succès !');
+
+    return $this->redirectToRoute('vehicules_list');
+}
+
+
+
+
+
+
+
+
+}
+
+
