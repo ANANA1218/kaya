21aff4c4d32b4d1ab9371597614f43a7be44c411
Annabelle <annabelle.bosse.pro@gmail.com>
2023-11-26 14:42:33 +0100

    resumer

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -13,7 +13,8 @@ use App\Repository\VehiculeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Commande;
 use App\Entity\Vehicule;
-
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use DateTime;
 
 class CommandeController extends AbstractController
 {
@@ -41,27 +42,42 @@ class CommandeController extends AbstractController
         return $panier;
     }
     
-    /**
-     * @Route("/reservation/{id}", name="reserver_vehicule")
-     */
-    public function reserverVehicule(Request $request, $id): Response
+
+
+
+
+    #[Route("/reservation/{id}", name:"reserver_vehicule")]
+    public function reserverVehicule(Request $request, $id, EntityManagerInterface $entityManager): Response
     {
+        // ... (récupération du véhicule par son ID)
         $vehicule = $this->vehiculeRepository->find($id);
-    
+
+        // Récupération des dates de début et fin de la requête POST
+        $dateDebut = $request->request->get('date_debut');
+        $dateFin = $request->request->get('date_fin');
+
+        // Ajout du véhicule au panier avec les dates de réservation
         $panier = $this->getOrCreatePanier($request);
-        $panier['vehicules'][] = $vehicule->getId();
-    
-        $session = $request->getSession();
-        $session->set('panier', $panier);
-    
+        $panier['vehicules'][] = [
+            'vehiculeId' => $vehicule->getId(),
+            'dateDebut' => $dateDebut,
+            'dateFin' => $dateFin,
+        ];
+        $request->getSession()->set('panier', $panier);
+
+        // Redirection vers la page du panier
         return $this->redirectToRoute('afficher_panier');
     }
     
-      /**
-     * @Route("/panier", name="afficher_panier")
-     */
-    public function afficherPanier(Request $request): Response
+     /**
+ * @Route("/panier", name="afficher_panier")
+ */
+public function afficherPanier(Request $request): Response
 {
+    $dateDebut = $request->query->get('date_debut');
+    $dateFin = $request->query->get('date_fin');
+dump($dateDebut, $dateFin);
+
     $panier = $this->getOrCreatePanier($request);
     $vehiculeRepository = $this->vehiculeRepository;
 
@@ -73,10 +89,24 @@ class CommandeController extends AbstractController
         }
     }
 
+    $user = $this->getUser();
+    $userName = $user ? $user->getNom() : null;
+    $userFirstName = $user ? $user->getPrenom() : null;
+    $userId = $user ? $user->getUserIdentifier() : null;
+
     return $this->render('panier/panier.html.twig', [
         'vehiculesPanier' => $vehiculesPanier,
+        'dateDebut' => $dateDebut,
+        'dateFin' => $dateFin,
+        'userName' => $userName,
+        'userFirstName' => $userFirstName,
+        'userId' => $userId,
     ]);
 }
+
+
+
+
 
 public function supprimerVehiculePanier(Request $request, $id): Response
 {
@@ -96,5 +126,33 @@ public function supprimerVehiculePanier(Request $request, $id): Response
 
     return $this->redirectToRoute('afficher_panier');
 }
+
+
+ /**
+     * @Route("/synthese_reservation/{id_vehicule}", name="synthese_reservation")
+     */
+    public function syntheseReservation(Request $request, $id_vehicule): Response
+    {
+        // Récupération du véhicule depuis la base de données
+        $vehicule = $this->vehiculeRepository->find($id_vehicule);
+
+        // Récupération des dates de début et de fin depuis la requête
+        $dateDebut = new DateTime($request->query->get('date_debut'));
+        $dateFin = new DateTime($request->query->get('date_fin'));
+
+        $dateDebutFormatted = $dateDebut->format('Y-m-d'); // Changez le format selon vos besoins
+        $dateFinFormatted = $dateFin->format('Y-m-d');
+
+        // Affichage de la page de résumé avec les détails
+        return $this->render('panier/panier.html.twig', [
+            'vehicule' => $vehicule,
+            'dateDebut' => $dateDebutFormatted,
+            'dateFin' => $dateFinFormatted,
+        ]);
+    }
+
+
+
+
 
 }
